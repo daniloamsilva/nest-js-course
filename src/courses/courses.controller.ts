7fd7431cadfadd47175cec7d8a1f0d9ada9e08ee
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-08-31 19:09:03 -0300

    Aula: Trabalhando com os parâmetros de uma rota

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get('list')
-  listAll() {
+  @Get('')
+  index() {
     return 'Lista de cursos';
+  }
+
+  @Get(':id')
+  show(@Param('id') id: string) {
+    return `Course #${id}`;
   }
 }
