0386c39f312d59a414f3e91e0697bdeb121ee32d
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-09-07 20:49:45 -0300

    Aula: Exemplo de teste e2e - Courses

---
+++ test/courses/courses.e2e-spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { CoursesModule } from '../../src/courses/courses.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [CoursesModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it.todo('POST /courses');
+});
