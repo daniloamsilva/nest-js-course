ba83e6b8823285a762fd5bc3fb97ae2811c21503
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-09-07 13:41:06 -0300

    Aula: Exemplo de teste unitário - CourseService p2

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CoursesService } from './courses.service';
 import { Course } from './entities/course.entity';
 import { Tag } from './entities/tag.entity';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+});
+
 describe('CoursesService', () => {
   let service: CoursesService;
+  let coursesRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,19 +22,33 @@ describe('CoursesService', () => {
         { provide: Connection, useValue: {} },
         {
           provide: getRepositoryToken(Course),
-          useValue: {},
+          useValue: createMockRepository(),
         },
         {
           provide: getRepositoryToken(Tag),
-          useValue: {},
+          useValue: createMockRepository(),
         },
       ],
     }).compile();
 
     service = module.get<CoursesService>(CoursesService);
+    coursesRepository = module.get<MockRepository>(getRepositoryToken(Course));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to find one course', async () => {
+    const courseId = '1';
+    const expectedCourse = {};
+
+    coursesRepository.findOne.mockReturnValue(expectedCourse);
+    const course = await service.show(courseId);
+    expect(course).toBe(expectedCourse);
+  });
+
+  it('should not be able to find a non-existent course', () => {
+    // code
   });
 });
