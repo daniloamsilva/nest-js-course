06ac037a850f25e2db420b7a655c72e6073da018
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-09-07 21:27:13 -0300

    Aula: Exemplo de teste e2e - configurando o banco

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -2,13 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { CoursesModule } from '../../src/courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [CoursesModule, TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'docker',
+        database: 'cursonestjs_test',
+        entities: [__dirname + '/**/*.entity.js'],
+        autoLoadEntities: true,
+        synchronize: true,
+      })],
     }).compile();
 
     app = moduleFixture.createNestApplication();
