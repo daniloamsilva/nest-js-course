f484393a788d39a251e2977d96fd4b2efe727570
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-09-22 22:47:48 -0300

    Aula: Criação de migrações com TypeORM v0.3.x

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,4 +1,9 @@
 import { DataSource } from 'typeorm';
+import { CreateCoursesTable1662430361433 } from './migrations/1662430361433-CreateCoursesTable';
+import { CreateTagsTable1662431300826 } from './migrations/1662431300826-CreateTagsTable';
+import { CreateCoursesTagsTable1662519008607 } from './migrations/1662519008607-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1662519250713 } from './migrations/1662519250713-AddCoursesIdToCoursesTagsTable';
+import { AddTagsIdToCoursesTagsTable1662519635607 } from './migrations/1662519635607-AddTagsIdToCoursesTagsTable';
 
 export const databaseProviders = [
   {
@@ -18,3 +23,20 @@ export const databaseProviders = [
     },
   },
 ];
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'cursonestjs',
+  entities: [__dirname + '/**/*.entity.js'],
+  migrations: [
+    CreateCoursesTable1662430361433,
+    CreateTagsTable1662431300826,
+    CreateCoursesTagsTable1662519008607,
+    AddCoursesIdToCoursesTagsTable1662519250713,
+    AddTagsIdToCoursesTagsTable1662519635607,
+  ],
+});
